b28bcd6ba4b7e6a507692de0c2e8c14176cdc608
napoleonbaley2011 <113143033+napoleonbaley2011@users.noreply.github.com>
2022-09-11 20:43:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -79,33 +79,86 @@ if (!empty($_SESSION['active'])) {
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login </title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets/css/tailwind.output.css" />
+    <script
+      src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
+      defer
+    ></script>
+    <script src="assets/js/init-alpine.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>LOGIN</title>
-</head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-<body>
-    <section id="container">
-        <form action="index.php" method="POST">
-            <h3>Login de Accesoooo</h3>
-            <img src="imagenes/imagen_rest_281_1.png" alt="Login" width="150px" height="100px">
-            <input type="text" name="usuario" placeholder="Nombre Usuario">
-            <input type="password" name="clave" placeholder="Contraseña">
+  </head>
+  <body>
+    <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+      <div
+        class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800"
+      >
+        <div class="flex flex-col overflow-y-auto md:flex-row">
+          <div class="h-32 md:h-auto md:w-1/2">
+            <img
+              aria-hidden="true"
+              class="object-cover w-full h-full dark:hidden"
+              src="assets/img/imagen_login.png"
+              alt="Office"
+            />
+           
+          </div>
+          
+          <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+            <form action="index.php" method="POST">
+              <div class="w-full">
+                
+                <img
+                    aria-hidden="true"
+                    class="object-cover w-full h-full dark:hidden"
+                    src="assets/img/logo.png"
+                    alt="Office"
+                />
+                <label class="block text-sm">
+                  <span class="text-gray-700 dark:text-gray-400">Usuario</span>
+                  <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-orange-400 focus:outline-none focus:shadow-outline-orange dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="Codigo Usuario" type="text" name="cod_usr" id="cod_usr"
+                  />
+                </label>
+                <label class="block mt-4 text-sm">
+                  <span class="text-gray-700 dark:text-gray-400">Contraseña</span>
+                  <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-orange-400 focus:outline-none focus:shadow-outline-orange dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="***************"
+                    type="password" name="pwd" id="pwd"
+                  />
+                </label>  
+                <input type="submit" value="ENTRAR" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-orange-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red">
 
-            <div class="alert"> <?php echo isset($alert) ? $alert : ''; ?></div>
-            <input type="submit" value="ACCEDER">
+                <hr class="my-8" />
 
-        </form>
-
-    </section>
-
-
-
-</body>
-
+                <p class="mt-4">
+                  <a
+                    class="text-sm font-medium text-gray-600 dark:text-black-400 hover:underline"
+                    href="./forgot-password.html"
+                  >
+                    ¿Olvidaste tu contraseña?
+                  </a>
+                </p>
+              
+              </div>
+            </form>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
